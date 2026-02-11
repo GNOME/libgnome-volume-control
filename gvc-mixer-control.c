GNOME/libgnome-volume-control

commit 8bf60eef4c7933d2f2eb7184240a2c2c7548de49

mixer-control: Reset `stream-id`s on UI devices
If we find a matching UI device for a stream matching card and
port and the same stream is also on another port, reset that
UI devices `stream-id`.

Since wireplumber 1.5.84 will merely flip the port but not the
`stream-id` we need to make sure the UI device loses its stream-id.
Otherwise `gvc_mixer_control_change_output` will try a
`gvc_mixer_stream_change_port` which seemingly succeeds but fails
silently as the port doesn't exist on that stream at the moment.

By resetting the `stream-id` we ensure that we take the
`gvc_mixer_control_change_profile_on_selected_device` code path in
`gvc_mixer_control_change_output` instead and thus the sink/port
gets properly selected.

Signed-off-by: Guido Günther <agx@sigxcpu.org>

gvc-mixer-control.c
@@ -1257,23 +1257,29 @@ match_stream_with_devices (GvcMixerControl    *control,
                                  stream_port->port,
                                  stream_card_id);
 
-                        if (stream_card_id == card_id &&
-                            g_strcmp0 (device_port_name, stream_port->port) == 0) {
-                                g_debug ("Match device with stream: We have a match with description: '%s', origin: '%s', cached already with device id %u, so set stream id to %i",
-                                         description,
-                                         origin,
-                                         gvc_mixer_ui_device_get_id (device),
-                                         stream_id);
+                        if (stream_card_id == card_id) {
+                                if (g_strcmp0 (device_port_name, stream_port->port) == 0) {
+                                        g_debug ("Match device with stream: We have a match with description: '%s', origin: '%s', cached already with device id %u, so set stream id to %i",
+                                                 description,
+                                                 origin,
+                                                 gvc_mixer_ui_device_get_id (device),
+                                                 stream_id);
 
-                                g_object_set (G_OBJECT (device),
-                                              "stream-id", stream_id,
-                                              NULL);
-                                in_possession = TRUE;
+                                        g_object_set (G_OBJECT (device),
+                                                      "stream-id", stream_id,
+                                                      NULL);
+                                        in_possession = TRUE;
+                                } else {
+                                        /* Other UI devices on that card that match the stream can't be valid */
+                                        if (device_stream_id == stream_id) {
+                                                g_object_set (G_OBJECT (device),
+                                                              "stream-id", GVC_MIXER_UI_DEVICE_INVALID,
+                                                              NULL);
+                                                g_debug ("Found another UI device for this stream: %d, resetting", stream_id);
+                                        }
+                                }
                         }
                 }
-
-                if (in_possession == TRUE)
-                        break;
         }
 
         return in_possession;

commit d2442f455844e5292cb4a74ffc66ecc8d7595a9f

mixer-control: Only stream-id for bluetooth devices only
They're the only ones that merely flip ports and clearing it for
other devices breaks e.g. lineout and headphone combinations.

Fixes: 8bf60ee ("mixer-control: Reset `stream-id`s on UI devices")

Closes: #40
Signed-off-by: Guido Günther <agx@sigxcpu.org>

gvc-mixer-control.c
@@ -1218,7 +1218,8 @@ add_stream (GvcMixerControl *control,
 static gboolean
 match_stream_with_devices (GvcMixerControl    *control,
                            GvcMixerStreamPort *stream_port,
-                           GvcMixerStream     *stream)
+                           GvcMixerStream     *stream,
+                           gboolean            is_bluetooth)
 {
         g_autoptr (GList)        devices = NULL;
         guint                    stream_card_id;
@@ -1279,7 +1280,7 @@ match_stream_with_devices (GvcMixerControl    *control,
                                         in_possession = TRUE;
                                 } else {
                                         /* Other UI devices on that card that match the stream can't be valid */
-                                        if (device_stream_id == stream_id) {
+                                        if (device_stream_id == stream_id && is_bluetooth) {
                                                 g_object_set (G_OBJECT (device),
                                                               "stream-id", GVC_MIXER_UI_DEVICE_INVALID,
                                                               NULL);
@@ -1390,7 +1391,7 @@ sync_devices (GvcMixerControl *control,
                 GvcMixerStreamPort *stream_port;
                 stream_port = n->data;
 
-                if (match_stream_with_devices (control, stream_port, stream))
+                if (match_stream_with_devices (control, stream_port, stream, is_bluetooth))
                         continue;
 
                 g_warning ("Sync_devices: Failed to match stream id: %u, description: '%s', origin: '%s'",